1ea1fb5a3659c734dbbce2e8906010b7a735ea15
unknown <dorynnamara@gmail.com>
2017-11-07 10:45:50 +0300

    modified viewspy

---
+++ views.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+
+if __name__ == "__main__":
+    app.run()
