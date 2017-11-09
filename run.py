b33357f9fdc47dc29b711758db1da3c56f55a7a4
unknown <dorynnamara@gmail.com>
2017-11-10 01:31:35 +0300

    implemented CRUD for recipes

--- run.py
+++ run.py
@@ -1,16 +1,17 @@
-from flask import Flask, render_template,flash, redirect, url_for, session, request, logging
+from flask import Flask, render_template, flash, redirect, url_for, session, request, logging
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from models.user import User
 from models.categories import Category
 from models.recipes import Recipe
+from functools import wraps
 
 
-USERS= {}
-CATEGORIES={}
-RECIPES={}
+USERS = {}
+CATEGORIES = {}
 
 recipes = {}
 app = Flask(__name__)
+
 
 class RegisterForm(Form):
     name = StringField('Name', [validators.Length(min=1, max=50)])
@@ -21,11 +22,20 @@ class RegisterForm(Form):
         validators.EqualTo('confirm', message='Passwords do not match')
     ])
     confirm = PasswordField('Confirm Password')
+
+
 class LoginForm(Form):
     name = StringField('Name', [validators.Length(min=1, max=50)])
     password = PasswordField('Password',)
-
-
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized, Please login', 'danger')
+            return redirect(url_for('login'))
+    return wrap
 
 @app.route('/')
 def home():
@@ -42,22 +52,25 @@ def register():
         username = form.username.data
         password = str(form.password.data)
 
-        user = User(username, email, password)#creating an instance of a user
+        # creating an instance of a user
+        user = User(username, email, password)
 
-        USERS[username] = user #storing a user object in the USERS dictionary
+        USERS[username] = user  # storing a user object in the USERS dictionary
 
         flash('You are now registered and can log in', 'success')
         return render_template('login.html')
-    #if a GET request
+    # if a GET request
     return render_template('register.html', form=form)
 # User login
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm(request.form)
     password = form.password.data
     if request.method == 'POST':
         username = str(request.form['username'])
-        #comparison if the input password is equal to the password used for registration
+        # comparison if the input password is equal to the password used for registration
         password_candidate = str(request.form['password'])
 
         for user in USERS.values():
@@ -68,71 +81,112 @@ def login():
                 flash('You are now logged in', 'success')
 
                 return redirect(url_for('dashboard'))
-            else:#if any of the two are not satisfied
+            else:  # if any of the two are not satisfied
                 error = 'Invalid login'
                 return render_template('login.html', error=error)
     return render_template("login.html")
 
 
 @app.route('/dashboard')
-def dashboard ():
-    return render_template('categories.html',categories = CATEGORIES)# we are passing the CATEGORIES dict to the template
+@is_logged_in
+def dashboard():
+    # Am are passing the CATEGORIES dict to the template
+    return render_template('categories.html', categories=CATEGORIES)
+
 
 @app.route('/add_category', methods=['GET', 'POST'])
+@is_logged_in
 def add_category():
     if request.method == 'POST':
-        #Adding the Category object to the CATEGORIES dict
-        CATEGORIES[request.form['title']] = Category(request.form['title'])
-        return render_template('categories.html', categories=CATEGORIES)
-    return redirect(url_for('dashboard'))#this is a get request
+        if not request.form['title'] in CATEGORIES:
+            if  request.form['title']  != " ":
+            # Adding the Category object to the CATEGORIES dict
+                CATEGORIES[request.form['title']] = Category(request.form['title'])
+                flash('category added successfully', 'success')
+                return render_template('categories.html', categories=CATEGORIES)
+        flash('category already exists','danger')
+    return render_template('add_category.html')  # this is a get request
 
 
 @app.route('/delete_category/<title>')
+@is_logged_in
 def delete_category(title):
     if title in CATEGORIES:
-        del CATEGORIES[title] # deleting a the value for title from the CATEGORIES dictionary
+        # deleting a the value for title from the CATEGORIES dictionary
+        del CATEGORIES[title]
+        flash('category delete successfully', 'success')
         return redirect(url_for('dashboard'))
 
 
 @app.route('/edit_category/<title>', methods=['GET', 'POST'])
-
+@is_logged_in
 def edit_category(title):
-    category = CATEGORIES[title]#category represents category object(value) to title
+    # category represents category object(value) to title
+    category = CATEGORIES[title]
 
     if request.method == 'POST':
-        category.title = request.form['title'] #what we put on the form is the title of the category object
+        # what we put on the form is the title of the category object
+        category.title = request.form['title']
+        flash('category updated ','success')
+        return render_template('categories.html', categories=CATEGORIES, title=category.title)
+    # IF its a get request we render the edit template to update the data on the form
+    return render_template('edit_category.html', title=category.title, category=Category(title))
 
-        return render_template('categories.html', categories = CATEGORIES, title= category.title)
-    #IF its a get request we render the edit template to update the data on the form
-    return render_template('edit_category.html', title = category.title,category=Category(title))
 
-@app.route('/add_Recipe/<title>',methods=['GET','POST'])
-def add_Recipe(title):
+@app.route('/view_recipes/<category_title>')
+@is_logged_in
+def view_recipes(category_title):
+    session['current_category_title'] = category_title
+    # am  passing the recipes dict to the template
+    return render_template('recipes.html', recipes=recipes)
+
+@app.route('/add_recipe', methods=['GET', 'POST'])
+@is_logged_in
+def add_recipe():
     if request.method == 'POST':
-        RECIPES[request.form['title']] = Recipe(request.form['title'])
-        return redirect(url_for('recipes_dashboard'))#this is a get request
-    return render_template('recipe_dashboard.html', recipes=RECIPES)
+        if not request.form['title'] in recipes:
+            recipes[request.form['title']] = Recipe(request.form['title'],request.form['description'])
+            flash('recipe category added successfully', 'success')
+            return render_template('recipes.html', recipes=recipes)
+        flash('recipe already exists', 'danger')
+    return render_template('add_recipe.html')
+
+@app.route('/delete_recipe/<title>')
+@is_logged_in
+def delete_recipe(title):
+    if title in recipes:
+        del recipes[title]
+    flash('recipe added successfully', 'success')
+    return render_template('recipes.html', recipes =recipes)
 
 
+@app.route('/edit_recipe/<title>', methods=['GET', 'POST'])
+@is_logged_in
+def edit_recipe(title):
+    recipe = recipes[title]
 
-@app.route('/recipes_dashboard/<title>')
-def recipes_dashboard(title):
-    return render_template('recipes.html',recipes=RECIPES)
+    if request.method == 'POST':
+        recipe.title = request.form['title']
+        recipe.description = request.form['description']
 
+        return render_template('recipes.html', recipes=recipes,title=title)
 
+    return render_template('edit_recipe.html', title=recipe.title, description=recipe.description)
 
-
-
-
-@app.route
+@app.route('/recipe/<title>', methods=['GET', 'POST'])
+@is_logged_in
+def recipe_details(title):
+    recipe = recipes[title]
+    return render_template("details.html", title=recipe.title, description=recipe.description)
 
 @app.route('/logout')
+@is_logged_in
 def logout():
-    session.clear() #clear the session
+    session.clear()  # clear the session
     flash('You are now logged out', 'success')
     return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
-    app.secret_key='sfxhecygdzfzrettzxgvbdsjdbshbv123'
+    app.secret_key = 'sfxhecygdzfzrettzxgvbdsjdbshbv123'
     app.run(debug=True)
