d5379479cc348eebf449bb0ae4c3ff92a21aac3b
unknown <dorynnamara@gmail.com>
2017-11-09 04:13:50 +0300

    separated models

---
+++ models/recipes.py
@@ -0,0 +1,4 @@
+class Recipe:
+  def __init__(self,title):
+        self.title = title
+        self.recipes =[]
--- models/user.py
+++ models/user.py
@@ -5,8 +5,6 @@ class User:
         self.email =email
         self.password =password
         self.recipe_categories =[]
-class category:
-
     def add_recipe_category(self,recipe_category):
         if recipe_category not in self.recipe_categories:
             self.recipe_categories.append(recipe_category)
@@ -29,39 +27,3 @@ class category:
         for item in self.recipe_categories:
             return self.recipe_categories
         return "recipe_categories is empty"
-
-class Recipe_category:
-
-    def __init__(self,title):
-        self.title = title
-        self.recipes =[]
-
-    def add_recipe(self, recipe):
-        if recipe not in self.recipes:
-            self.recipes.append(recipe)
-            return "recipe added succesfully"
-        return "recipe already exists"
-
-    def edit_recipe(self,newrecipe,oldrecipe):
-        if oldrecipe in self.recipes:
-            self.recipes= [newrecipe for oldrecipe in self.recipes]
-            return "recipe added successfully"
-        return "no recipe to edit"
-
-    def delete_recipe(self,recipe):
-        if recipe in self.recipes:
-            self.recipes.remove(recipe)
-            return "recipe deleted"
-        return "No recipe to delete"
-
-    def view_recipe(self,recipe):
-        for recipe in self.recipes:
-            return self.recipes
-        return "no recipe found"
-
-class Recipe:
-
-    def __init__(self, title, description):
-        self.recipe_title = title
-        self.recipe_description = description
-
